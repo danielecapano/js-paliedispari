0d09fbfeb613b401b8c6979437a5e777ac5a1365
danielecapano <daniele.capano@gmail.com>
2023-10-12 15:54:22 +0200

    FIrst commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+//ESERCIZIO PARLOLE PALINDROME
+
+let word = prompt("Inserisci una parola");
+
+if (isPalindrome(word)) {
+  console.log(`${word} è una parola palindroma`);
+} else {
+  console.log(`${word} non è una parola palindroma`);
+}
+
+//ESERCIZIO PARI O DISPARI
+
+let userChoice = "";
+
+do {
+  userChoice = prompt("Scegli pari o dispari");
+} while (userChoice !== "pari" || userChoice !== "dispari");
+console.log(userChoice);
+let userNumber;
+
+do {
+  userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
+} while (userNumber < 1 || userNumber > 5);
+console.log(userNumber);
+const pcNumber = getRandomInt(1, 5);
+console.log(pcNumber);
+
+if (
+  (isSumEven(userNumber, pcNumber) && userChoice === "pari") ||
+  (!isSumEven(userNumber, pcNumber) && userChoice === "dispari")
+) {
+  console.log("Complimenti hai vinto");
+} else {
+  console.log("Mi dispiace hai perso");
+}
+
+function isPalindrome(string) {
+  let word = string.toLowerCase();
+
+  //metodo 1
+  let reverseWorld = word.split("").reverse().join("");
+
+  //metodo 2 con ciclo for
+
+  // for (let i = word.length - 1; i >= 0; i--) {
+  //   reverseWorld += word[i];
+  // }
+
+  if (word === reverseWorld) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function isSumEven(num1, num2) {
+  let sum = num1 + num2;
+  if (sum % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
